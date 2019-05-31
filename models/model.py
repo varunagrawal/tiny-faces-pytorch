7024c2799c4b8c73cfc734d4147e9d80c8bb6f16
Varun Agrawal <varagrawal@gmail.com>
2019-05-31 16:18:18 -0400

    remove bias from prediction layers in model

--- models/model.py
+++ models/model.py
@@ -11,21 +11,28 @@ class DetectionModel(nn.Module):
 
     def __init__(self, base_model=resnet101, num_templates=1, num_objects=1):
         super().__init__()
-        output = (num_objects + 4)*num_templates  # 4 is for the bounding box offsets
+        # 4 is for the bounding box offsets
+        output = (num_objects + 4)*num_templates
         self.model = base_model(pretrained=True)
-        
-        self.score_res3 = nn.Conv2d(in_channels=512, out_channels=output, kernel_size=1)
-        self.score_res4 = nn.Conv2d(in_channels=1024, out_channels=output, kernel_size=1)
 
-        self.score4 = nn.ConvTranspose2d(in_channels=output, out_channels=output, kernel_size=4, stride=2, padding=1)
+        self.score_res3 = nn.Conv2d(in_channels=512, out_channels=output,
+                                    kernel_size=1, padding=0)
+        self.score_res4 = nn.Conv2d(in_channels=1024, out_channels=output,
+                                    kernel_size=1, padding=0)
+
+        self.score4_upsample = nn.ConvTranspose2d(in_channels=output, out_channels=output,
+                                                  kernel_size=4, stride=2, padding=1, bias=False)
         self._init_bilinear()
+
+    def _init_weights(self):
+        pass
 
     def _init_bilinear(self):
         """
         Initialize the ConvTranspose2d layer with a bilinear interpolation mapping
         :return:
         """
-        k = self.score4.kernel_size[0]
+        k = self.score4_upsample.kernel_size[0]
         factor = np.floor((k+1)/2)
         if k % 2 == 1:
             center = factor
@@ -33,20 +40,22 @@ class DetectionModel(nn.Module):
             center = factor + 0.5
         C = np.arange(1, 5)
 
-        f = np.zeros((self.score4.in_channels, self.score4.out_channels, k, k))
+        f = np.zeros((self.score4_upsample.in_channels,
+                      self.score4_upsample.out_channels, k, k))
 
-        for i in range(self.score4.out_channels):
+        for i in range(self.score4_upsample.out_channels):
             f[i, i, :, :] = (np.ones((1, k)) - (np.abs(C-center)/factor)).T @ \
                             (np.ones((1, k)) - (np.abs(C-center)/factor))
 
-        self.score4.weight = torch.nn.Parameter(data=torch.Tensor(f))
+        self.score4_upsample.weight = torch.nn.Parameter(data=torch.Tensor(f))
 
     def learnable_parameters(self, lr):
         parameters = [
-            {'params': self.model.parameters(), 'lr': lr},  # Be T'Challa. Don't freeze.
+            # Be T'Challa. Don't freeze.
+            {'params': self.model.parameters(), 'lr': lr},
             {'params': self.score_res3.parameters(), 'lr': 0.1*lr},
-            {'params': self.score_res4.parameters(), 'lr': 1*lr},
-            {'params': self.score4.parameters(), 'lr': 0}  # freeze UpConv layer
+            {'params': self.score_res4.parameters(), 'lr': 1*lr},  # freeze UpConv layer
+            {'params': self.score4_upsample.parameters(), 'lr': 0}
         ]
         return parameters
 
@@ -65,15 +74,16 @@ class DetectionModel(nn.Module):
         x = self.model.layer3(x)
         res4 = x
 
-        x = self.model.layer4(x)
-        res5 = x
+        # x = self.model.layer4(x)
+        # res5 = x
 
         score_res3 = self.score_res3(res3)
 
         score_res4 = self.score_res4(res4)
-        score4 = self.score4(score_res4)
+        score4 = self.score4_upsample(score_res4)
 
-        if not self.training:  # then we need to do some fancy cropping to accomodate the difference in image sizes
+        # We need to do some fancy cropping to accomodate the difference in image sizes in eval
+        if not self.training:
             # from vl_feats DagNN Crop
             cropv = score4.size(2) - score_res3.size(2)
             cropu = score4.size(3) - score_res3.size(3)
@@ -88,8 +98,7 @@ class DetectionModel(nn.Module):
         else:
             # match the dimensions arbitrarily
             score4 = score4[:, :, 0:score_res3.size(2), 0:score_res3.size(3)]
-            
+
         score = score_res3 + score4
 
         return score
-
