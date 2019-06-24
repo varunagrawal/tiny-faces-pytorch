fixed double normalization issue

datasets/wider_face.py
@@ -214,17 +214,9 @@ class WIDERFace(dataset.Dataset):
             # NOTE Return only the image and the image path.
             # Use the eval_tools to get the final results.
             if self.transforms is not None:
-                print("Performaing transforms in val")
-                print(self.transforms)
-                img = self.transforms(image)
+                # Only convert to tensor since we do normalization after rescaling
+                img = transforms.functional.to_tensor(image)
 
-            # if self.debug:
-            #     bboxes = datum['bboxes']
-            #     # Visualize stuff
-            #     im = Image.fromarray(img.permute((1, 2, 0)).numpy().astype('uint8'), 'RGB')
-            #     visualize.visualize_bboxes(image, np.asarray([]))
-            #     # and now we exit
-            #     exit(0)
 
             return img, datum['img_path']
 

evaluate.py
@@ -39,7 +39,7 @@ def dataloader(args):
                                      std=[0.229, 0.224, 0.225])
     val_transforms = transforms.Compose([
         transforms.ToTensor(),
-        # normalize
+        normalize
     ])
 
     val_loader, templates = get_dataloader(args.dataset, args,
@@ -89,7 +89,6 @@ def run(model, val_loader, templates, prob_thresh, nms_thresh, device, split, de
             print(img.shape)
             mean = torch.as_tensor([0.485, 0.456, 0.406], dtype=torch.float32, device=img.device)
             std = torch.as_tensor([0.229, 0.224, 0.225], dtype=torch.float32, device=img.device)
-            # img.mul_(std[:, None, None]).add_(mean[:, None, None])
             im = Image.fromarray((img[0] * 255).permute((1, 2, 0)).numpy().astype('uint8'), 'RGB')
             print(dets.shape)
             visualize.visualize_bboxes(im, dets)